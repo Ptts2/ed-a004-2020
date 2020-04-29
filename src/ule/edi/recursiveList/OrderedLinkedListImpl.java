f0a2b2eb9fa5db0c848faee1e97047144528f2fc
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2020-04-29 12:47:18 +0200

    Metodo add

--- src/ule/edi/recursiveList/OrderedLinkedListImpl.java
+++ src/ule/edi/recursiveList/OrderedLinkedListImpl.java
@@ -4,24 +4,52 @@ public class OrderedLinkedListImpl<T extends Comparable<? super T>> extends
 		AbstractLinkedListImpl<T> implements OrderedListADT<T> {
 
 	public OrderedLinkedListImpl() {
-		// Vacía
+		// Vacia
 	}
 
 	public OrderedLinkedListImpl(T... v) {
-		// Añade todos los elementos del array 'v'
+		// A�ade todos los elementos del array 'v'
 		for (T Vi : v) {
 			add(Vi);
 		}
 	}
 
 	
-
-	
 	@Override
 	public void add(T element) {
 		//TODO RECURSIVO
 		
+		if(element == null)
+			throw new NullPointerException();
 	
+		if(this.front == null) {
+			this.front = new Node<T>(element);
+			
+		}else if (this.front.elem.compareTo(element) >=0) {
+			Node<T> aux = new Node<T>(element);
+			aux.next=front;
+			front = aux;
+		}else {
+			addRec(front, element);
+		}
+		
+		
+		
+	}
+	
+	private void addRec(Node<T> nodo, T element) {
+		
+		if(nodo.next == null) {
+			nodo.next= new Node<T>(element);
+		
+		}else if(nodo.next.elem.compareTo(element) >=0) {
+			Node<T> anyadir = new Node<T>(element);
+			anyadir.next = nodo.next;
+			nodo.next = anyadir;
+		}else {
+			addRec(nodo.next, element);
+		}
+		
 	}
 	
 	
